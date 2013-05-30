28646d14c821c6ee85fd7b230226d6cd28bd70c3
Guille <guille@ggalmazor.com>
2013-05-30 20:00:47 +0200

    Ahora guardamos los eventos en Mongo

--- src/main/java/net/programania/axon/AxonBootstrap.java
+++ src/main/java/net/programania/axon/AxonBootstrap.java
@@ -1,5 +1,6 @@
 package net.programania.axon;
 
+import com.mongodb.Mongo;
 import net.programania.aggregates.TransactionAggregate;
 import net.programania.handlers.TransactionEventsHandler;
 import org.axonframework.commandhandling.CommandBus;
@@ -12,19 +13,30 @@ import org.axonframework.eventhandling.SimpleEventBus;
 import org.axonframework.eventhandling.annotation.AnnotationEventListenerAdapter;
 import org.axonframework.eventsourcing.EventSourcingRepository;
 import org.axonframework.eventstore.EventStore;
-import org.axonframework.eventstore.fs.FileSystemEventStore;
-import org.axonframework.eventstore.fs.SimpleEventFileResolver;
+import org.axonframework.eventstore.mongo.DefaultMongoTemplate;
+import org.axonframework.eventstore.mongo.MongoEventStore;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.io.File;
+import java.net.UnknownHostException;
 
 public class AxonBootstrap {
+  private static final Logger log = LoggerFactory.getLogger(AxonBootstrap.class);
+
   public static CommandGateway bootstrap() {
     // let's start with the Command Bus
     CommandBus commandBus = new SimpleCommandBus();
     // the CommandGateway provides a friendlier API
     CommandGateway commandGateway = new DefaultCommandGateway(commandBus);
     // we'll store Events on the FileSystem, in the "events/" folder
-    EventStore eventStore = new FileSystemEventStore(new SimpleEventFileResolver(new File("./events")));
+    Mongo mongo = null;
+    try {
+      mongo = new Mongo("127.0.0.1");
+    } catch (UnknownHostException e) {
+      log.error("No hemos podido enchufarnos a Mongo", e);
+      throw new RuntimeException(e); // BOOOOOM!
+    }
+    EventStore eventStore = new MongoEventStore(new DefaultMongoTemplate(mongo));
     // a Simple Event Bus will do
     EventBus eventBus = new SimpleEventBus();
     // we need to configure the repository
